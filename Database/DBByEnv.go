c74d1ac69028c55c1a167d0174c33441d7798790
Evan Haston <ehaston@darkmatterit.io>
2020-02-06 14:48:07 -0500

    feature: added ability to enable and disable databases

--- Database/DBByEnv.go
+++ Database/DBByEnv.go
@@ -66,6 +66,37 @@ func DatabaseAllIP() []string {
 
 }
 
+func DatabaseAllHostEnabled() []string {
+	var databaseip  []string
+	db := DatabaseInitAll("/etc/dm/","GenService", "GenService.username", "GenService.password", "GenService.dbhost")
+	var db_ip string;
+	//var databaseip string;
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer tx.Rollback()
+	stmt, err := tx.Query("SELECT db_name FROM clarity_tools.tbl_database_info where Enabled  = 1")
+	if err != nil {
+		log.Fatal("There was a problem up the db.")
+	}
+	defer stmt.Close()
+
+
+
+	for stmt.Next() {
+		err := stmt.Scan(&db_ip)
+		if err != nil {
+			log.Fatal(err)
+		}
+		databaseip = append(databaseip, db_ip)
+	}
+
+	return databaseip
+
+}
+
 
 func DatabaseDemo() []string {
 	var databaseip  []string
